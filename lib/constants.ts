29a2b16c08a2d082f5345fb9f1c20f738e12c9e8
guchuwanjiku <peterjoseph084@gmail.com>
2025-02-05 00:26:07 +0300

    Product-details-Page

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,4 @@ export const APP_SLOGAN=
     process.env.NEXT_PUBLIC_APP_SLOGAN ||'Spend less, enjoy more'
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Next js App'
+export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE || 9)

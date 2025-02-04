0e3222219d8106ce7c1afe034934dc25fd0c9d6e
guchuwanjiku <peterjoseph084@gmail.com>
2025-02-04 07:33:33 +0300

    Best-selling-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -59,6 +59,8 @@ export default async function Page() {
 
   const todaysDeals = await getProductsByTag({tag: 'todays-deal'})
 
+  // const bestSellingProducts = await getProductsByTag({tag: 'best-Seller'})
+
   return (
   <>
     <HomeCarousel items={data.carousels} />
@@ -68,6 +70,12 @@ export default async function Page() {
       <Card className="w-full rounded-none">
         <CardContent className="p-4 items-center gap-3">
           <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+        </CardContent>
+      </Card>
+
+      <Card className="w-full rounded-none">
+        <CardContent className="p-4 items-center gap-3">
+          <ProductSlider title={"Best Selling Products"} products={todaysDeals} hideDetails />
         </CardContent>
       </Card>
     </div>

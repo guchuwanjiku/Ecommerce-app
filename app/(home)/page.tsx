aaa010786c8fe5323192e9b9d5395e6c4364913e
guchuwanjiku <peterjoseph084@gmail.com>
2025-02-05 08:56:09 +0300

    Create-browsing-history

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import data from "@/lib/data";
 import { toSlug } from "@/lib/utils";
 import { Card, CardContent } from '@/components/ui/card';
 import ProductSlider from "@/components/shared/product/product-slider";
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
 
 export default async function Page() {
   const categories = (await getAllCategories()).slice(0, 4)
@@ -79,6 +80,10 @@ export default async function Page() {
         </CardContent>
       </Card>
     </div>
+    
+    <div className='p-4 bg-background'>
+        <BrowsingHistoryList />
+      </div>
   </>
   )
 }

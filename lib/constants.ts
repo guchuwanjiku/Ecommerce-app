ad012686776b70fea4880da2120377280674fd95
guchuwanjiku <peterjoseph084@gmail.com>
2025-02-06 20:49:37 +0300

    Implement-add-to-cart

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,7 @@ export const APP_SLOGAN=
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Next js App'
 export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE || 9)
+
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+)

d09bc18ccb131b4ffbb958ed74e39310a15db629
guchuwanjiku <peterjoseph084@gmail.com>
2025-02-11 14:44:46 +0300

    Signin-user

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,8 +3,14 @@ export const APP_SLOGAN=
     process.env.NEXT_PUBLIC_APP_SLOGAN ||'Spend less, enjoy more'
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Next js App'
+
+export const APP_COPYRIGHT =
+    process.env.NEXT_PUBLIC_APP_COPYRIGHT || 
+    `Copyright © ${new Date().getFullYear()} ${APP_NAME}. All rights reserved`
+
 export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE || 9)
 
 export const FREE_SHIPPING_MIN_PRICE = Number(
   process.env.FREE_SHIPPING_MIN_PRICE || 35
 )
+
